efa8e43782cbcb6aa7e81542cf937c21a94c7ec3
Jacob1225 <carlone.jacob@gmail.com>
2020-04-14 20:30:09 +0000

    added a post request that takes the the longurl from the form and redirects the user to a new window

--- express_server.js
+++ express_server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const shortUrl = function generateRandomString() {
-    return Math.random().toString(36).substring(2, 8);
-}
+//const shortUrl = function generateRandomString() {
+    //return Math.random().toString(36).substring(2, 8);
+//}
 
 app.set("view engine", "ejs");
 
@@ -17,16 +17,25 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Helper Functions
+
+const addNewUrl = longUrl => {
+    
+    const shortUrl = Math.random().toString(36).substring(2, 8);
+
+    //add it to the urlDatabase
+
+    urlDatabase[shortUrl] = longUrl;
+
+    return shortUrl;
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
-
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
   app.get("/urls", (req, res) => {
@@ -43,9 +52,17 @@ app.get("/urls.json", (req, res) => {
     res.render("urls_show", templateVars);
   });
 
-  app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  app.get("/u/:shortURL", (req, res) => {
+     const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  });
+
+  app.post('/urls', (req, res) => {
+  
+    const longUrl = req.body['longURL'];
+    const shortUrl = addNewUrl(longUrl);
+  
+    res.redirect(`urls/${shortUrl}`);
   });
 
 app.listen(PORT, () => {
